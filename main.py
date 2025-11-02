2780f2b7f2f0458f1fd83ed4a2c2e1484dc9b631
FARHAN AKMAL <akmalf416@gmail.com>
2025-11-02 19:52:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -76,4 +76,8 @@ def favicon():
     )
 
 if __name__ == "__main__":
-       app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+       if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+    app.run(host="0.0.0.0", port=8000)
+
